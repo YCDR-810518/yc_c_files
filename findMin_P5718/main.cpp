f5ec9eac78e9f9325ac8739dee0ea08178bcf15d
YCDR-810518 <yaochen1234567@outlook.com>
2025-10-21 10:50:39 +0800

    Initial Commit

---
+++ findMin_P5718/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  findMin_P5718
+//
+//  Created by 姚晨 on 2025/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
